f3c8d89bd7da7154ff23337569f5d24e4053c82b
John Rearden <reardenjohn@gmail.com>
2022-10-02 00:58:04 +0100

    Populate basic drum editor template with drumloop, track and instrument

--- beats_app/views.py
+++ beats_app/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Drumloop, Track, Instrument
 
 class LoopEditor(View):
     def get(self, request, name='default_name', *args, **kwargs):
-        loop = Drumloop.objects.get_or_create(name=name)[:1]
-        print(f"number of loops returned = {len(loop)}")
+        query_set = Drumloop.objects.all()
+        loop = get_object_or_404(query_set, name='default_name')
+        track = Track.objects.select_related()[:1]
+        instrument = Instrument.objects.select_related()[0]
+        print(f"Instrument: {instrument}")
+        print(loop.created_on)
+        print(f"Tracks in {loop} : {track}")
 
         return render(
             request,
             "loop_editor.html",
             {
-                "obj": loop,
+                "loop": loop,
+                "tracks": track, 
+                "instrument": instrument,
             }
         )
