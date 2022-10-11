61a1a337926270c58ec51c105477c0bd4d4dfd8f
John Rearden <reardenjohn@gmail.com>
2022-10-11 23:44:28 +0100

    Attempt deployment

--- just_beats/settings.py
+++ just_beats/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
-
+""" X_FRAME_OPTIONS = 'SAMEORIGIN'
+ """
 ALLOWED_HOSTS = ['just-beats.herokuapp.com', 'localhost']
 
 # Application definition
