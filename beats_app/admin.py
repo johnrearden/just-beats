dfc55196e381d34ec62b2076ff0bdde2c2aecf0c
John Rearden <reardenjohn@gmail.com>
2022-10-22 17:47:35 +0100

    Add rating field to Drumloop model, with max and min validators, add to admin view list also

--- beats_app/admin.py
+++ beats_app/admin.py
@@ -3,8 +3,8 @@ from .models import Drumloop, Track, Instrument
 
 @admin.register(Drumloop)
 class DrumloopAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'creator', 'created_on', 'allow_copy', 'tempo')
-    list_editable = ('name', 'creator', 'allow_copy', 'tempo')
+    list_display = ('id', 'name', 'creator', 'created_on', 'allow_copy', 'tempo', 'rating')
+    list_editable = ('name', 'creator', 'allow_copy', 'tempo', 'rating')
     search_fields = ('name',)
 
 @admin.register(Track)
--- beats_app/models.py
+++ beats_app/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
 
 class Drumloop(models.Model):
-    name = models.CharField(max_length=50, unique=True, default="brand_new_loop")
-    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="drumloops", default=1)
+    name = models.CharField(max_length=50, unique=True,
+                            default="brand_new_loop")
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="drumloops", default=1)
     created_on = models.DateTimeField(auto_now_add=True)
     allow_copy = models.BooleanField(default=True)
-    tempo = models.IntegerField(default=120, validators=[MaxValueValidator(200), MinValueValidator(60)])
+    rating = models.IntegerField(default=0, validators=[
+        MinValueValidator(0), MaxValueValidator(5)
+    ])
+    tempo = models.IntegerField(default=120, validators=[
+                                MaxValueValidator(200), MinValueValidator(60)])
 
     def __str__(self):
         return f"{self.name}, tempo={self.tempo}, {'(can be copied)' if self.allow_copy else '(copying not allowed)'}"
@@ -22,11 +31,14 @@ class Instrument(models.Model):
 
 
 class Track(models.Model):
-    drumloop = models.ForeignKey(Drumloop, on_delete=models.CASCADE, related_name="tracks")
-    instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE, related_name="tracks")
+    drumloop = models.ForeignKey(
+        Drumloop, on_delete=models.CASCADE, related_name="tracks")
+    instrument = models.ForeignKey(
+        Instrument, on_delete=models.CASCADE, related_name="tracks")
     beats = models.CharField(max_length=32)
     beat_volumes = models.CharField(max_length=32)
-    track_volume = models.IntegerField(default=10, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    track_volume = models.IntegerField(
+        default=10, validators=[MaxValueValidator(10), MinValueValidator(0)])
 
     def __str__(self):
         return f"{self.instrument}: {self.beats}, {self.beat_volumes}, trk_vol={self.track_volume}, belongs to {self.drumloop.name}"
