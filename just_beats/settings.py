64ec5e96d16b1bc3c817d9435df0abc270394940
John Rearden <reardenjohn@gmail.com>
2022-10-23 19:49:52 +0100

    Deployment commit

--- just_beats/settings.py
+++ just_beats/settings.py
@@ -149,7 +149,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
+                    os.path.join(BASE_DIR, 'media')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
