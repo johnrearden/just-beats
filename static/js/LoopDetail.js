4983c8b5f6361fe830b576dd7aedd84198d6cfbe
John Rearden <reardenjohn@gmail.com>
2022-12-07 20:45:05 +0000

    Fix site logo animation

--- static/js/LoopDetail.js
+++ static/js/LoopDetail.js
@@ -123,12 +123,12 @@ const fireAnimation = (beatIndex) => {
     const loopName = document.getElementById('loop-name-heading');
     if (beatIndex % 16 === 0) {
         siteLogo.classList.remove('transition-on');
-        siteLogo.style.transform = 'scale(1.1, 1.1)';
+        siteLogo.style.fontSize = '1.1em';
         loopName.style.transform = 'scale(1.1, 1.1)';
         loopName.classList.remove('transition-on');
     }
     if (beatIndex % 16 === 8) {
-        siteLogo.style.transform = 'scale(1.0, 1.0)';
+        siteLogo.style.fontSize = '1.0em';
         siteLogo.classList.add('transition-on');
         loopName.style.transform = 'scale(1.0, 1.0)';
         loopName.classList.add('transition-on');
